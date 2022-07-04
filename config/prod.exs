5e663d5a4d02a4708107cb08b285fbf4a13f192b
luc50514@gmail.com <luc50514@gmail.com>
2022-07-04 10:33:11 -0400

    heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -19,17 +19,13 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :socceracademy, SocceracademyWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
-#
+     config :socceracademy, SocceracademyWeb.Endpoint,
+       ...,
+       url: [scheme: "https",
+        host: "socceracademy.herokuapp.com/",
+        port: 443],
+        force_ssl: [rewrite_on: [:x_forwarded_proto]],
+       #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
 # and clients may not be supported. You can set it to
